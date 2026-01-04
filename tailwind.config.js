2597d8eefaf45c360a3363f8c1f9ea4e133a50aa
Raja Nagarajan <rajanagarajan@Nagarajas-MacBook-Pro.local>
2026-01-04 18:23:11 -0500

    Switch Tailwind to PostCSS version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./app/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}'],
-  theme: { extend: {} },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
