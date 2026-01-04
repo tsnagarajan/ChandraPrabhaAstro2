ee752aac2ce41c987cbeede06e61a7b36848c916
Raja Nagarajan <rajanagarajan@Nagarajas-MacBook-Pro.local>
2026-01-04 18:11:37 -0500

    Disable LightningCSS for Linux build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    optimizeCss: false   // Disable LightningCSS for Linux builds
+  },
+
   serverExternalPackages: ['swisseph'],
+
   typescript: {
     ignoreBuildErrors: true
   },
+
   eslint: {
     ignoreDuringBuilds: true
   },
+
   webpack: (config) => {
     config.externals.push({
       swisseph: 'commonjs swisseph'
